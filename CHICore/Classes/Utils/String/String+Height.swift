7af14d71a2d679842851e3534c12d28719786e29
Maksims Puskels <maksims.puskels@chi.lv>
2019-04-04 17:18:48 +0300

    Height with constraint width added.

---
+++ CHICore/Classes/Utils/String/String+Height.swift
@@ -0,0 +1,23 @@
+//
+//  String+Height.swift
+//  CHICore
+//
+//  Created by McSims on 04/04/2019.
+//
+
+import Foundation
+
+extension String {
+
+    public func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return boundingBox.height
+    }
+
+    public func width(withConstraintedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: font], context: nil)
+        return boundingBox.width + 25
+    }
+}
